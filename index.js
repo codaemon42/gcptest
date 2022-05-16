07e2e1523d1242b8ba23d5ff37ef3f5f65ad0e6f
codaemon42 <naimhassanjcc2542@gmail.com>
2022-05-16 15:46:27 +0600

    C:/Program Files/Git/tests

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.get("/", async (req, res) => {
 });
 app.get("/test", async (req, res) => {
       res.json({ status: "testing" });
-    });
+});
+
+app.get("/test2", async (req, res) => {
+      res.json({ status: "testing2" });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
