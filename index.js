59d5ba2dbc31e084a5a513d3b09c228333e369da
codaemon42 <naimhassanjcc2542@gmail.com>
2022-05-16 15:40:42 +0600

    added

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 8000;
 app.get("/", async (req, res) => {
   res.json({ status: "OK" });
 });
+app.get("/test", async (req, res) => {
+      res.json({ status: "testing" });
+    });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
